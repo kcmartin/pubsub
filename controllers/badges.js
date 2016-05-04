d7eedfb12dec44f4364675c04810e1d2e8afbb57
kcmartin <kcmartin@users.noreply.github.com>
2016-05-04 15:54:58 -0700

    update model and controller + app.js

--- controllers/badges.js
+++ controllers/badges.js
@@ -18,6 +18,6 @@ exports.save = function(req, res, next) {
 /**
 *  Send badges to pubsub socket in model
 */
-exports.send = function() {
-
+exports.send = function(req, res, next) {
+  next();
 };

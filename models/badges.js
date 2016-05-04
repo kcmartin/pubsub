6c2efbc60d9066b0f9d09ea1fffbf75c3e244f2e
kcmartin <kcmartin@users.noreply.github.com>
2016-05-04 13:54:16 -0700

    Updated models/badges.js

--- models/badges.js
+++ models/badges.js
@@ -9,5 +9,12 @@ var redis require('../lib/redis');
 */
 
 exports.save = function(badges, callback) {
+  if (!badges.length) return callback(null, null);
+  var badge = badges.pop();
+  redis.lpush('badges', JSON.stringify(badge), function(err){
+    if (err) return callback(err, null);
+      /* recursive function */
+      exports.save(badges, callback);
 
+  });
 };

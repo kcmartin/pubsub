0c4fce29f9a2a30d8742c492f737867f4b4ef7c4
kcmartin <kcmartin@users.noreply.github.com>
2016-05-04 13:34:41 -0700

    Added models/badges.js

---
+++ models/badges.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var redis require('../lib/redis');
+
+/**
+*  Save badges to database
+*  @param {Array} badges
+*  @param {Function} callback
+*/
+
+exports.save = function(badges, callback) {
+
+};
